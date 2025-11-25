8f25557b7bb23606bd0bc43e8f8f5d8c8f9f20e9
DeepanshuKumar071 <deepanshukr704@gmail.com>
2025-11-26 00:56:32 +0530

    DAY - 41

---
+++ DAY-41/Q81.c
@@ -0,0 +1,19 @@
+// #include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int count = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    while (str[count] != '\0')
+    {
+        count++;
+    }
+
+    printf("Total characters = %d\n", count);
+
+    return 0;
+}
