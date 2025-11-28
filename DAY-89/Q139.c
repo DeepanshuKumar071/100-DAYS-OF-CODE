3c61e3a014d066e903c04c796cd9629b7509d8c2
DeepanshuKumar071 <deepanshukr704@gmail.com>
2025-11-28 23:28:18 +0530

    DAY-89

---
+++ DAY-89/Q139.c
@@ -0,0 +1,20 @@
+// Show that enums store integers by printing assigned values.
+#include <stdio.h>
+
+enum Status
+{
+    SUCCESS = 10,
+    FAILURE,
+    TIMEOUT = 50,
+    UNKNOWN
+};
+
+int main()
+{
+    printf("SUCCESS = %d\n", SUCCESS);
+    printf("FAILURE = %d\n", FAILURE);
+    printf("TIMEOUT = %d\n", TIMEOUT);
+    printf("UNKNOWN = %d\n", UNKNOWN);
+
+    return 0;
+}
